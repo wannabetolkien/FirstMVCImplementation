0e38f428d4ce532a0addfb88fd5b6de3584a5a24
Nabeel Akhter <114768159+wannabetolkien@users.noreply.github.com>
2023-11-20 22:39:09 +0530

    Update View.java
    Instead of view for Inventory System only, Billing has also been added to the code.

--- View.java
+++ View.java
@@ -1,6 +1,7 @@
+import java.util.*;
 public class View {
     public static void main (String[] args) throws Exception {
-        Controller jantaKirana=new Controller();
-        jantaKirana.view(jantaKirana);
+        Controller janta=new Controller();
+        janta.netView(janta);
     }
 }
